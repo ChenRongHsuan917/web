0ebc51400ff14408810fb946b9328a3ab48618fd
ChenRongHsuan917 <sherry225863@gmail.com>
2022-04-12 18:12:50 +0800

    add music brb.

--- YouTube/playList.js
+++ YouTube/playList.js
@@ -1,6 +1,7 @@
 //YouTube Video ID
 let playList = [
-    "q1gm4u-3i2s",//前奏
+   "q1gm4u-3i2s",//新高前奏
+    "U7Wod1UVrZ0",//brb. do me right
     "EJud0U7m1C8",//元智大學107級畢業季限定歌曲 YZU Graduation MV ─ 讓回憶留在倒數以前
     "cd60NmOEdvU",//骨盆端正
     "q1gm4u-3i2s",//新高27th畢業候選歌 【Blossom / 初綻 】
@@ -11,6 +12,7 @@ let playList = [
 
 //播放起訖秒數
 let playTime = [
+    [0,30],
     [0,30],
     [75,98],
     [124,135],//2:04 215
