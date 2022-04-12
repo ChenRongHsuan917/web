efc8e237685c11c43f81392b52ad856cb44ca55e
ChenRongHsuan917 <sherry225863@gmail.com>
2022-04-12 18:02:37 +0800

    rpg_new

--- RPG_game/main.js
+++ RPG_game/main.js
@@ -5,16 +5,19 @@ let imgMountain, imgMain, imgEnemy;
 //currentImgMainX, currentImgMainY - 決定主角所在座標
 //imgMountain, imgMain, imgEnemy - 障礙物, 主角, 敵人的圖片物件
 
-const gridLength = 200;
+const gridLength = 100;//200
 
 //網頁載入完成後初始化動作
 $(function(){
 
     //設定地形、擺上主角
     mapArray = [ //0-可走,1-障礙,2-終點,3-敵人
-         [0,1,1],
-         [0,0,0],
-         [3,1,2]
+         [0,1,1,1,0,1],
+         [0,0,0,1,0,3],
+         [0,1,0,3,0,1],
+         [3,1,0,0,0,0],
+         [0,1,1,0,1,3],
+         [1,0,0,0,0,2]
     ];
     ctx = $("#myCanvas")[0].getContext("2d");
     
@@ -97,7 +100,7 @@ $(document).on("keydown",function(event){
     }
 
     //確認目標位置不會超過地圖
-    if(targetImg.x<=400 && targetImg.x>=0 && targetImg.y<=400 && targetImg.y>=0){
+    if(targetImg.x<=800 && targetImg.x>=0 && targetImg.y<=800 && targetImg.y>=0){
        targetBlock.x = targetImg.y / gridLength;
        targetBlock.y = targetImg.x / gridLength;
     }else{
@@ -118,7 +121,7 @@ $(document).on("keydown",function(event){
                   break;
 
              case 1: // 有障礙物(不可移動)
-                  $("#talkBox").text("有山");
+                  $("#talkBox").text("此處不通");
                   break;
 
              case 2: // 終點(可移動)
@@ -128,7 +131,7 @@ $(document).on("keydown",function(event){
                   break;
 
              case 3: // 敵人(不可移動)
-                  $("#talkBox").text("哈摟");
+                  $("#talkBox").text("敵人!快逃");
                   break;
         }
     }
