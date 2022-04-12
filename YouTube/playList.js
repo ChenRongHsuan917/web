b22f9b1cdf4b2827a373e24b9483b1cc48184f6d
ChenRongHsuan917 <sherry225863@gmail.com>
2022-04-12 19:06:27 +0800

    add yt mussic moddddd

--- YouTube/playList.js
+++ YouTube/playList.js
@@ -1,21 +1,43 @@
 //YouTube Video ID
 let playList = [
-   "Qmt9Bzrf29k",//2 soon
-   "o3zVtbw0VDM",//blue
+   "xK93ninHyss",//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 強尼 
+   "U7Wod1UVrZ0",//brb. - do me right 
+   "Cere6zvT2TY&list=LL&index=17",//COVID-19
+   "o3zVtbw0VDM",//keshi - blue
+   "ZwIw1WiJ-csv",//夢幻女神朱碧石Beauty Lo你幹嘛!
+   "MxFexdep0Zo",//超人特攻隊-小傑的攻擊(台語版)
+   "Qmt9Bzrf29k",//keshi - 2 soon
+   "xK93ninHyss",//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 圍牆男
+   "EJud0U7m1C8",//元智大學107級畢業季限定歌曲 YZU Graduation MV ─ 讓回憶留在倒數以前
+   "xK93ninHyss",//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 太空沙拉
+   "TBkybgllGRA",//超跑情人夢
+   "EqwDaHDsOH4",//Hojean - Over 85
+   "ZlkL2HHLbMY",//Fern. - Kaori
+   "xK93ninHyss",//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 女生
    "q1gm4u-3i2s",//新高前奏
    "cd60NmOEdvU",//骨盆端正
-   "q1gm4u-3i2s",//新高27th畢業候選歌 【Blossom / 初綻 】
-   "EJud0U7m1C8",//元智大學107級畢業季限定歌曲 YZU Graduation MV ─ 讓回憶留在倒數以前
-   "MxFexdep0Zo"//超人特攻隊-小傑的攻擊(台語版)
+   "q1gm4u-3i2s"//新高27th畢業候選歌 【Blossom / 初綻 】
+   
 ];
 
 //播放起訖秒數
 let playTime = [
-    [0,3],//
-    [0,3],//
-    [0,30],
-    [124,135],//2:04 215
-    [276,357],//4:36 5:57
-    [75,98],
-    [130,150]
+    [12,39],//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 強尼
+    [99,116]//brb.-do me right
+    [122,137],//COVID-19
+    [109,120],//keshi-blue 
+    [100,115],//夢幻女神朱碧石Beauty Lo你幹嘛!
+    [130,150],//超人特攻隊-小傑的攻擊(台語版)
+    [154,177],//keshi-2 soon
+    [321,340],//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 圍牆男 521 540
+    [75,98],//元智大學107級畢業季限定歌曲 YZU Graduation MV ─ 讓回憶留在倒數以前
+    [161,190],//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 太空沙拉 337 431
+    [68,74],//超跑情人夢
+    [59,80]//Hojean - Over 85
+    [70,77],//Fern. - Kaori
+    [217,271],//NTU Hip-Hop 台大嘻研 2019 Cypher [文藝復興] 女生
+    [0,30],//新高前奏
+    [124,135],//骨盆端正
+    [276,357]//新高27th畢業候選歌 【Blossom / 初綻 】
+    
 ];
